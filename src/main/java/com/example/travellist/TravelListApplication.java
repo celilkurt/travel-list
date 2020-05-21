5eb6ca6747c2f56b22b203c0af6f5eb2d57fa724
celil <celilkurt82@gmail.com>
2020-05-21 17:15:52 +0300

    first commit

---
+++ src/main/java/com/example/travellist/TravelListApplication.java
@@ -0,0 +1,21 @@
+package com.example.travellist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class TravelListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelListApplication.class, args);
+	}
+
+
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ src/main/java/com/example/travellist/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.travellist.controller;
+
+
+import com.example.travellist.dao.UserRepository;
+import com.example.travellist.entity.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(UserRepository userRepository,
+                          BCryptPasswordEncoder bCryptPasswordEncoder){
+
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+
+    }
+
+    @RequestMapping(value = "/sign-up", method = RequestMethod.POST)
+    public void signUp(@RequestBody User user){
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+
+}
